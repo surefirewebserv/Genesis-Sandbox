9bc449c35c46046a5f7f9d0973725f45183f3e4b
surefirewebserv <jon@fromyouto.us>
2012-08-28 11:17:19 -0400

    1140 and responsive
    Added Responsiveness and 1140 grid

--- functions.php
+++ functions.php
@@ -28,6 +28,24 @@ $pullmenu = '<a href="#" id="pull" class="closed">Menu</a>';
     return sprintf( '%2$s<div id="nav" class="nav-responsive">%1$s%3$s</div>%4$s', $nav, genesis_structural_wrap( 'nav', 'open', 0 ), $pullmenu, genesis_structural_wrap( 'nav', 'close', 0 ) );
 
 }
+
+//Responsive Nav
+add_filter( 'genesis_do_subnav', 'override_do_subnav', 10, 3 );
+function override_do_subnav($nav_output, $nav, $args) {
+$pullmenu = '<a href="#" id="subpull" class="closed">Sub Menu</a>';
+	
+    //$args['menu_class'] .= ' nav-responsive'; 
+
+    // check which function should be used to build the nav
+    // rebuild the nav using the updated arguments
+    
+        $nav = wp_nav_menu( $args );
+    
+    // return the modified result
+    return sprintf( '%2$s<div id="menu-secondary-navigation" class="subnav-responsive">%1$s%3$s</div>%4$s', $nav, genesis_structural_wrap( 'nav', 'open', 0 ), $pullmenu, genesis_structural_wrap( 'nav', 'close', 0 ) );
+
+}
+
 //Add Script for Responsive Nav
 function my_responsive_nav() {
 	wp_enqueue_script(
