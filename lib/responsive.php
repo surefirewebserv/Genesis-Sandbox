e94ecf7f0052fab1e497a8529bee12d2ed8d7479
surefirewebserv <jon@fromyouto.us>
2012-08-29 12:16:23 -0400

    Added Dev Version
    Thank you WP_Smith!

--- lib/responsive.php
+++ lib/responsive.php
@@ -33,13 +33,15 @@ $pullmenu = '<a href="#" id="subpull" class="closed">Sub Menu</a>';
 
 }
 
-//Add Script for Responsive Nav
-function my_responsive_nav() {
-	wp_enqueue_script(
-		'responsive-nav',
-		get_stylesheet_directory_uri() . '/lib/js/responsive-nav.js',
-		array('jquery')
-	);
-	  wp_enqueue_style( 'responsive', CHILD_URL . '/responsive.css');
+add_action( 'wp_enqueue_scripts', 'wps_child_script' );
+/**
+ * Enqueues Appropriate Scripts when needed based on Debugging.
+ * Assumes that the normal *.js is the minified version & *-dev.js is beautified version.
+ *
+ * @uses wp_enqueue_script() WP adds JS to page safely.
+ */
+function wps_child_script() {
+	$suffix = ( WP_DEBUG || WP_SCRIPT_DEBUG ) ? '-min.js' : '.js';
+	wp_enqueue_script( 'responsive-nav', get_stylesheet_directory_uri() . '/lib/js/responsive-nav' . $suffix, array( 'jquery' ) , '1.0.0', true );
+	wp_enqueue_style( 'responsive', CHILD_URL . '/responsive.css');
 }
-add_action('wp_enqueue_scripts', 'my_responsive_nav');
