03fe1482cae32a116327ff60fe3b3f6e82546d97
surefirewebserv <jon@fromyouto.us>
2012-09-04 14:22:34 -0400

    Auto Update Functionality and Minor Style Changes

--- functions.php
+++ functions.php
@@ -2,6 +2,7 @@
 /** Start the engine */
 require_once( get_template_directory() . '/lib/init.php' );
 require_once( 'lib/responsive.php');
+include("lib/update_notifier.php");
 
 /** Child theme (do not remove) */
 define( 'CHILD_THEME_NAME', 'Sample Child Theme' );
