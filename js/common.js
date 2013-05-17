25146a2878f3a37f22539f6e5cda04d0c0b31ab1
surefirewebserv <jperez@surefirewebservices.com>
2013-05-17 16:31:11 -0400

    Changed Onclick to Hover in CSS

--- js/common.js
+++ js/common.js
@@ -1,20 +1,17 @@
-jQuery(document).ready(function (e) {
-    e("ul.menu-mobile").before('<div id="primary-menu-toggle" class="menu-toggle primary"><a class="toggle-switch show" href="#"><span>Show Menu</span></a></div>');
-    e("#primary-menu-toggle .show").click(function () {
-        if (e(".menu-mobile").is(":hidden")) {
-            e(".menu-mobile").slideDown(500);
-            e(this).attr("class", "toggle-switch hide").attr("title", "Hide Menu");
-            e("#primary-menu-toggle span").replaceWith("<span>Hide Menu</span>")
+jQuery(document).ready(function ($) {
+    $("ul.menu-mobile").before('<div id="primary-menu-toggle" class="menu-toggle primary"><a class="toggle-switch show" href="#"><span>Show Menu</span></a></div>');
+    $("#primary-menu-toggle .show").click(function () {
+        if ($(".menu-mobile").is(":hidden")) {
+            $(".menu-mobile").slideDown(500);
+            $(this).attr("class", "toggle-switch hide").attr("title", "Hide Menu");
+            $("#primary-menu-toggle span").replaceWith("<span>Hide Menu</span>")
         } else {
-            e(".menu-mobile").hide(500);
-            e(this).attr("class", "toggle-switch show").attr("title", "Show Menu");
-            e("#primary-menu-toggle span").replaceWith("<span>Show Menu</span>")
+            $(".menu-mobile").hide(500);
+            $(this).attr("class", "toggle-switch show").attr("title", "Show Menu");
+            $("#primary-menu-toggle span").replaceWith("<span>Show Menu</span>")
         }
     });
-    e(".menu-mobile .menu-item").click(function (t) {
-        t.preventDefault();
-        e(this).children(".sub-menu").slideToggle()
-    });
+    
     jQuery(".scroll, .gototop a").click(function (e) {
         e.preventDefault();
         jQuery("html,body").animate({
